98588b5516c07b23782afd076477d4677db5eda2
Nick Adams <nick.adams@mediaforge.com>
2014-07-17 12:47:52 -0600

    Cleaning file

--- keyboard/node-3/keymap.c
+++ keyboard/node-3/keymap.c
@@ -48,23 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* 5 */   { KC_##K5A, KC_##K5B, KC_##K5C, KC_NO,    KC_NO,    KC_NO,    KC_##K5G, KC_NO,    KC_NO,    KC_NO,    KC_##K5K, KC_##K5L, KC_##K5M, KC_##K5N, KC_##K5O, KC_##K5P, KC_##K5Q } \
 }
 
-/*
-#define KEYMAP_ANSI( \
-    K0A,      K0C, K0D, K0E, K0F, K0G, K0H, K0I, K0J, K0K, K0L, K0M, K0N,      K0O, K0P, K0Q, \
-    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N,      K1O, K1P, K1Q, \
-    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N,      K2O, K2P, K2Q, \
-    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L,      K3N,                     \
-    K4A,      K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L,      K4N,           K4P,      \
-    K5A, K5B, K5C,                K5G,                K5K, K5L, K5M, K5N,      K5O, K5P, K5Q  \
-) KEYMAP( \
-    K0A,      K0C, K0D, K0E, K0F,      K0H, K0I, K0J, K0K, K0L, K0M, K0N,      K0O, K0P, K0Q, \
-    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N,      K1O, K1P, K1Q, \
-    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N,      K2O, K2P, K2Q, \
-    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L,      K3N,                     \
-    K4A,      K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L,      K4N,           K4P,      \
-    K5A, K5B, K5C,                K5G,                K5K, K5L, K5M, K5N,      K5O, K5P, K5Q  \
-)*/
-
 #include "keymap_ansi.h"
 
 #define KEYMAPS_SIZE    (sizeof(keymaps) / sizeof(keymaps[0]))
