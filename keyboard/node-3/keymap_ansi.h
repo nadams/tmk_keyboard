152b9031c6b6cceaddf5dcf7491f3cae0012c5c1
Nick Adams <nick.adams@mediaforge.com>
2014-10-24 13:23:21 -0600

    Setting colemak backspace to escape

--- keyboard/node-3/keymap_ansi.h
+++ keyboard/node-3/keymap_ansi.h
@@ -5,7 +5,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,       PSCR,SLCK,BRK,  \
         GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,      INS, HOME,PGUP, \
         TAB, Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,BSLS,      DEL, END, PGDN, \
-        BSPC,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,                       \
+        ESC, A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     ENT,                       \
         LSFT,     Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,     RSFT,           UP,        \
         LCTL,LGUI,LALT,               SPC,                RALT,RGUI,FN0, RCTL,      LEFT,DOWN,RGHT
     ),
